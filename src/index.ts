3034423a4bd4837431234f734503c5b8bb0f7694
“lragji” <22937426+LRagji@users.noreply.github.com>
2024-10-22 16:50:56 +0530

    Exported Injectable constructor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { kWayMerge } from "./k-way-merge/k-way-merge";
-import { StatefulProxyManager } from "./stateful-threads/stateful-proxy-manager";
-import { StatefulRecipient } from "./stateful-threads/stateful-recipient";
+import { InjectableConstructor } from "./injectable-constructor/injectable-constructor.js";
+import { kWayMerge } from "./k-way-merge/k-way-merge.js";
+import { StatefulProxyManager } from "./stateful-threads/stateful-proxy-manager.js";
+import { StatefulRecipient } from "./stateful-threads/stateful-recipient.js";
 
 export {
     kWayMerge,
     StatefulRecipient,
-    StatefulProxyManager
+    StatefulProxyManager,
+    InjectableConstructor
 }
